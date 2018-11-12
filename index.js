949df768e830f93c6df43503beeda4a6f3c296b6
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-12 17:59:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const logDriversByHometown = function(drivers, location) {
 const driversByRevenue = function(drivers) {
   const newDrivers = [];
   drivers.sort(function(d1, d2) {
-    return newdrivers.push(d1.revenue - d2.revenue);
+    return newDrivers.push(d1.revenue - d2.revenue);
   })
   return newDrivers
 }
