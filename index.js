70d0878eaadc1dc748688e17c39ea2c655391225
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-12 18:29:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const driversByName = function(drivers) {
 
 const totalRevenue = function(drivers) {
   return drivers.reduce(function(acc, driver) {
-    console.log(driver.revenue.type)
+    console.log(tyeOf(driver.revenue))
     return acc + driver.revenue
     
   })
